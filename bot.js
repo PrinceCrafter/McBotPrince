7ff4e793301825f61f5be54cc36f4a0a221de768
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-03 22:31:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,486 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Scrpit By ┃ KillerFox`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Bot By ┃ KillerFox ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` Bot By KillerFox | $help `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
 
+client.on('message', function(msg) {
+if(msg.content.startsWith ('$Waht')) {
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+      .addField(':globe_with_meridians:** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+      .addField(':medal:** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+      .addField(':red_circle:**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField(':large_blue_circle:**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+      .addField(':pencil:**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+      .addField(':microphone:**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+      .addField(':crown:**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+      .addField(':id:**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+      .addField(':date:**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+      msg.channel.send({embed:embed});
+    }
+  });
+  
+  var Himo_04 = ["https://f.top4top.net/p_682it2tg6.png","https://e.top4top.net/p_682a1cus5.png","https://d.top4top.net/p_682pycol4.png","https://c.top4top.net/p_682vqehy3.png","https://b.top4top.net/p_682mlf9d2.png","https://a.top4top.net/p_6827dule1.png","https://b.top4top.net/p_682g1meb10.png","https://a.top4top.net/p_682jgp4v9.png","https://f.top4top.net/p_682d4joq8.png","https://e.top4top.net/p_6828o0e47.png","https://d.top4top.net/p_6824x7sy6.png","https://c.top4top.net/p_682gzo2l5.png","https://b.top4top.net/p_68295qg04.png","https://a.top4top.net/p_682zrz6h3.png","https://f.top4top.net/p_6828vkzc2.png","https://e.top4top.net/p_682i8tb11.png","https://f.top4top.net/p_8816hvic1.png","https://d.top4top.net/p_882020461.png","https://e.top4top.net/p_882s3ftn1.png","https://a.top4top.net/p_882eg9c51.png","https://c.top4top.net/p_882xkcqd1.png","https://f.top4top.net/p_882w7pdi1.png","https://a.top4top.net/p_882gcpmo1.png"]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith('$لو خيروك')) {
+         var cat = new Discord.RichEmbed()
+.setImage(Himo_04[Math.floor(Math.random() * Himo_04.length)])
+message.channel.sendEmbed(cat);
+    }
+});
+  
+  
+  const Sra7a = [
+    'صراحه  |  صوتك حلوة؟',
+    'صراحه  |  التقيت الناس مع وجوهين؟',
+    'صراحه  |  شيء وكنت تحقق اللسان؟',
+    'صراحه  |  أنا شخص ضعيف عندما؟',
+    'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+    'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+    'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+    'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+    'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+    'صراحه  |  أشجع شيء حلو في حياتك؟',
+    'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+    'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+    'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+    'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+    'صراحه  |  نظرة و يفسد الصداقة؟',
+    'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+    'صراحه  |  شخص معك بالحلوه والمُره؟',
+    'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
+    'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+    'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+    'صراحه  |  ابيع المجرة عشان؟',
+    'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+    'صراحه  |  مع مين ودك تنام اليوم؟',
+    'صراحه  |  صدفة العمر الحلوة هي اني؟',
+    'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+    'صراحه  |  صفة تحبها في نفسك؟',
+    'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+    'صراحه  |  تصلي صلواتك الخمس كلها؟',
+    'صراحه  |  ‏تجامل أحد على راحتك؟',
+    'صراحه  |  اشجع شيء سويتة بحياتك؟',
+    'صراحه  |  وش ناوي تسوي اليوم؟',
+    'صراحه  |  وش شعورك لما تشوف المطر؟',
+    'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+    'صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  اي الدول تتمنى ان تزورها؟',
+    'صراحه  |  متى اخر مره بكيت؟',
+    'صراحه  |  تقيم حظك ؟ من عشره؟',
+    'صراحه  |  هل تعتقد ان حظك سيئ؟',
+    'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+    'صراحه  |  كلمة تود سماعها كل يوم؟',
+    'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+    'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+    'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+    'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+    '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+    'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+    '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+    '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+    '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+    '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+    '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+    '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
+     '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+    '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+    '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+    'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+    '‏صراحه  |  ما هو عمرك الحقيقي؟',
+    '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+]
+  client.on('message', message => {
+if (message.content.startsWith('$صراحه')) {
+    if(!message.channel.guild) return message.reply('** This command only for servers **');
+ var client= new Discord.RichEmbed()
+ .setTitle("لعبة صراحة ..")
+ .setColor('RANDOM')
+ .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+ .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                 .setTimestamp()
+ 
+  message.channel.sendEmbed(client);
+  message.react("??")
+}
+});
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+     if (message.content === "$servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+
+client.on('message', msg => {
+  if (msg.content === '$ping') {
+    msg.reply('Pong!');
+  }
+});
+if (message.content === '$help2') {
+              var embed  = new Discord.RichEmbed()
+                .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
+				.addField("**Bot**" ," :heart: Bot By KillerFox_:heart: ")
+                .addField("broadcast (bc)" ,"**الاستخدام:** ``~broadcast <الرساله> , $bc <الرساله>``")
+                .addField("**BAN**" ,"**الاستخدام:** ``~ban <المستخدم>``")
+                .addField("**KICK**" ,"**الاستخدام:** ``~kick <المستخدم> ``")
+                .addField("**ِAVATAR**" ,"**الاستخدام:** ``~avatar``")
+                .addField("**INFO**", "**الأستخدام :** ``~info``")
+                .addField("**SAY**" ,"**الاستخدام:** ``~say <الرساله>``")
+                .addField("**ID**" ,"**ال��ستخدام:** ``~id``")
+                .addField("**SERVER**" ,"**الاستخدام:** ``~server``")
+                .addField("**INVITE**" ,"**الاستخدام:** ``~invite <لأضافه البوت لأى سيرفر>``")
+                .addField("**SUPPORT**" ,"**الاستخدام:** ``~suppport <سيرفر دعم الفنى>``")
+                .addField("**QA**" ,"**الاستخدام:** ``~qa <السؤال>``  ")
+                .addField("**CLEAR**" ,"**الاستخدام:** ``~clear <العدد>``")
+                .addField("**PING**", "**الأستخدام:** ``~ping``")
+                .addField("**SERVERNAME**", "**الأستخدام:** ``~servername``")
+                .addField("**CUT_TWEET**", "**الاستخدام** ``~ct``")
+                .addField("**TWEET**", "**الاستخدام** ``~tweet <الرساله>``")
+                .addField("**NO INVITE LINKE**","تم اضافة خاصية منع الانفيتات ")
+                .addField("**LOGIN**" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
+                .setColor('RANDOM')
+.setColor('RANDOM')
+  message.author.sendEmbed(embed);
+    }
+});
+client.on('message', message => {
+  if(message.content === ('clear')) {
+  let modRole = message.guild.roles.find("name", "Admin");
+  if (!modRole) return message.reply('You do not have Admin Role'); {
+    }
+  const params = message.content.split(" ").slice(1)
+    let messagecount = parseInt(params[0]);
+    message.channel.fetchMessages({limit: messagecount})
+        .then(messages => message.channel.bulkDelete(messages));
+  }
+});
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+      if(!message.member.hasPermission('ADMINISTRATOR'))
+        message.delete()
+    return message.reply(`** No Invite Links :angry: ! **`)
+    }
+});
+client.on('message', msg => {
+  if (msg.content === '$invite') {
+    msg.reply('https://discordapp.com/api/oauth2/authorize?client_id=460797113643696129&permissions=8&scope=bot');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === '$help') {
+    msg.reply('**:heart:  شوف الخاص :heart:**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '$help1') {
+    msg.reply('**:heart:  شوف الخاص :heart:**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '$help3') {
+    msg.reply('**:heart: قريبا :heart:**');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === '$help2') {
+    msg.reply('**:heart:  شوف الخاص :heart:**');
+  }
+});
+
+// Your Avatar URL!
+client.on('message', message => {
+    if (message.content === "$Avatar") {
+    message.reply(message.author.avatarURL); 
+    }
+});
+client.on('message', msg => {
+  if (msg.content === '$suppport') {
+    msg.reply('السيرفر للمساعده,https://discord.gg/a5T4zQ');
+  }
+});
+    client.on('message', message => {
+     if (message.content === "$id") {
+     let embed = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)  
+  .setAuthor(message.author.username)
+.setDescription("معلومات عن الحــساب")
+               .setFooter(`Name Bot.`, '')
+  .setColor("#9B59B6")
+  .addField("اســـم الحســاب", `${message.author.username}`)
+  .addField('كود الحساب الخاص', message.author.discriminator)
+  .addField("الرقـــم الشـــخصي", message.author.id)
+  .addField('بــــوت', message.author.bot)
+  .addField("تاريخ التسجيل", message.author.createdAt)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+var prefix = "$";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+  });
+
+client.on('message', message => {
+    if (message.content === 'help') {
+    	message.reply('help');
   	}
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'هلا') {
+              message.channel.send(':heart: هلا فيك :heart:');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+  client.on("message", message => {
+ if (message.content === "$help") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─══════ {✯FoxBot✯} ══════─')
+	  .addField('**➺ $help1 ➺**', `** ⦁ أوامر عامة ⦁ **`)
+	  .addField('**➺ $help2 ➺**', `** ⦁قائمة الاكواد ⦁ **`)
+	  .addField('**➺ $help3 ➺**', `⦁ Soon ⦁`)
+      .setFooter('─══════ {✯FoxBot✯} ══════─')
+  message.author.send({embed});
+  }
+});
+
+
+  client.on("message", message => {
+ if (message.content === "$help-js") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('**$help-js-source ➺',` ⦁ قسم السورس الأساسي ⦁**`)
+	  .addField('**$help-js-bc ➺',` ⦁ قسم أكواد البرودكاست ⦁**`)
+	  .addField('**$help-js-welcome ➺',` ⦁ قسم أكواد الترحيب ⦁**`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+
+  client.on("message", message => {
+ if (message.content === "$help-js-bc") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('**$help-js-bc-1 ➺',` ⦁ جميع البرودكاست  ⦁**`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+
+  client.on("message", message => {
+ if (message.content === "$help-js-bc-1") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('**❖ برودكاست + للكل + مطور ➺',` ⦁  https://hastebin.com/bipanureqa.js ⦁**`)
+	  .addField('**❖ برودكاست + للكل + غير مطور ➺',` ⦁  https://hastebin.com/kuvoruzowe.cs ⦁**`)
+	  .addField('**❖  برودكاست + للأونلاين + مع منشن + غير مطور ➺',` ⦁  https://hastebin.com/zujuvupali.php ⦁**`)
+	  .addField('**❖ برودكاست + للكل + مع منشن + غير مطور ➺',` ⦁  https://hastebin.com/zojokunayo.php ⦁**`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+
+
+   client.on("message", message => {
+ if (message.content === "$help-js-source") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('**$help-js-source-1 ➺',` ⦁ السورس الأساسي⦁**`)
+	  .addField('**$help-js-source-2 ➺',` ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**`)
+	  .addField('**$help-js-source-3 ➺',` ⦁ السورس الأساسي مع الستريمنق ومعلومات البوت ⦁**`) 
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+   client.on("message", message => {
+ if (message.content === "$help-js-source-1") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('**❖ كود السورس الأساسي ',`https://hastebin.com/xumiferaru.coffeescript**`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+   client.on("message", message => {
+ if (message.content === "$help-js-source-2") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('** ❖ السورس الأساسي مع الستريمنق ومعلومات البوت',` https://hastebin.com/idotifusid.coffeescript **`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+   client.on("message", message => {
+ if (message.content === "$help-js-source-3") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+	  .addField('** ❖ السورس الأساسي مع الواتشينق ',`https://hastebin.com/jivizupafi.coffeescript **`)
+      .setFooter('─═════════ {✯FoxBot✯} ════════════─')
+  message.author.send({embed});
+  }
+});
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === '$by') {
+              message.channel.send('***:heart:*** **Server By KillerFox** ***:heart:***');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'hla') {
+              message.channel.send('**:heart: hla fek :heart:**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === '$bot') {
+              message.channel.send('***:heart:*** **Bot By KillerFox** ***:heart:***');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'KillerFox_') {
+              message.channel.send('**:heart: حالين مشغول بس يكمل يرد:heart:** ');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'السلام عليكم ') {
+              message.channel.send('**:heart: وعليكم السلام حبي:heart:**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'باي') {
+              message.channel.send('**:heart: لله معاك :heart:**');
+              message.channel.sendFile("https://lyrics-words.co/wp-content/uploads/2016/07/20160727-179.gif");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'برب') {
+              message.channel.send('**:heart: الله معاك :heart:** ');
+              message.channel.sendFile("https://lyrics-words.co/wp-content/uploads/2016/07/20160727-179.gif");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'brb') {
+              message.channel.send('**:heart: الله معاك :heart:**');
+              message.channel.sendFile("https://lyrics-words.co/wp-content/uploads/2016/07/20160727-179.gif");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'hi') {
+              message.channel.send('**:heart: Welcome :heart:**');
+              message.channel.sendFile("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8KNSG9XDY6qOJ8G8IRcLiIUDD-dCY7wCzKwIs8Idi5Y55qOxGgg");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === '$Dev') {
+              message.channel.send('***:heart:*** **Dev Server KillerFox** ***:heart:***');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
 });
 
 // THIS  MUST  BE  THIS  WAY
